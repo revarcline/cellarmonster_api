a669b9b85f8b4ea6c315980b3dcfa1868b8f38d1
Alex Cline <rev.a.r.cline@gmail.com>
2021-04-26 10:40:27 -0500

    yee

--- db/migrate/20210426144927_create_bottles_producers_join_table.rb
+++ db/migrate/20210426144927_create_bottles_producers_join_table.rb
@@ -2,7 +2,7 @@ class CreateBottlesProducersJoinTable < ActiveRecord::Migration[6.1]
   def change
     create_join_table :bottles, :producers do |t|
       t.index :bottle_id
-      t.index :producers_id
+      t.index :producer_id
     end
   end
 end
