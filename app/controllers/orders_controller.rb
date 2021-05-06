20b888146b32557db33041a2562af1584e7c346f
Alex Cline <rev.a.r.cline@gmail.com>
2021-05-05 19:04:06 -0500

    playing with order serializer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+  def index
+    orders = Order.order(created_at: :desc)
+    render json: OrderSerializer.new(orders).serializable_hash.to_json
+  end
+
+  def create
+    order = Order.create(order_params)
+    render json: OrderSerializer.new(order).serializable_hash.to_json
+  end
+
+  protected
+
+  def order_params
+    params.require(:order).permit(:quantity, :bottle_id, :user_id)
+  end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,7 @@
 class OrderSerializer
   include JSONAPI::Serializer
-  attributes :id, :quantity, :bottle
+  attributes :id, :quantity, :bottle, :user
+  attribute :bottle_producer do |order|
+    order.bottle.producer.name
+  end
 end
