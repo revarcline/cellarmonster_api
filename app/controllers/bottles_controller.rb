ebd47413fab419e6a756eb0ad47901fd997d7b6a
Alex Cline <rev.a.r.cline@gmail.com>
2021-05-08 18:01:46 -0500

    taking those params to the gym to get big and strong

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -8,4 +8,34 @@ class BottlesController < ApplicationController
     bottle = [Bottle.find(params[:id])]
     render json: BottleSerializer.new(bottle).serializable_hash.to_json
   end
+
+  def create
+    bottle = Bottle.create(bottle_params)
+    render json: BottleSerializer.new(bottle).serializable_hash.to_json
+  end
+
+  protected
+
+  def bottle_params
+    params.require(:bottle).permit(:name,
+                                   :other_country,
+                                   :new_producer,
+                                   :appellation,
+                                   :region,
+                                   :new_varietal,
+                                   :color,
+                                   :sparkling,
+                                   :price,
+                                   :vintage,
+                                   :notes,
+                                   :sku,
+                                   :inventory,
+                                   :format,
+                                   {
+                                     country: {},
+                                     producer: {},
+                                     varietals: {},
+                                     bins: {}
+                                   })
+  end
 end
