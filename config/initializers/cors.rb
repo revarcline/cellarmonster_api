492d71d13729e56cab74efad0264dab7bbe66993
Alex Cline <rev.a.r.cline@gmail.com>
2021-06-11 13:59:40 -0500

    updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://cellarmonster.buckar.ooo/'
+    origins 'cellarmonster.buckar.ooo', 'cellarmonster.herokuapp.com'
 
     resource '*',
              headers: :any,
